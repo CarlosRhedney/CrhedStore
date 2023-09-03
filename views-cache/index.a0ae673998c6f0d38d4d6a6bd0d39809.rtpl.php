9300570dcfb957a19e1447df75d5b0e40e14fc17
Carlos Rhedney <twisterpsa@hotmail.com>
2023-09-03 13:24:35 -0300

    Adding products in page

--- views-cache/index.a0ae673998c6f0d38d4d6a6bd0d39809.rtpl.php
+++ views-cache/index.a0ae673998c6f0d38d4d6a6bd0d39809.rtpl.php
@@ -2,43 +2,18 @@
         	<!-- Slider -->
 			<div class="block-slider block-slider4">
 				<ul class="" id="bxslider-home4">
+                    <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
 					<li>
-						<img src="/assets/site/img/h4-slide.png" alt="Slide">
+						<img src="/assets/site/img/h4-slide7.png" alt="Slide">
 						<div class="caption-group">
 							<h2 class="caption title">
-								iPhone <span class="primary">6 <strong>Plus</strong></span>
+								<?php echo htmlspecialchars( $value1["product"], ENT_COMPAT, 'UTF-8', FALSE ); ?> <span class="primary"><?php echo formatPrice($value1["price"]); ?></span>
 							</h2>
 							<h4 class="caption subtitle">Dual SIM</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+							<a class="caption button-radius" href="/products/<?php echo htmlspecialchars( $value1["url"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><span class="icon"></span>Comprar</a>
 						</div>
 					</li>
-					<li><img src="/assets/site/img/h4-slide2.png" alt="Slide">
-						<div class="caption-group">
-							<h2 class="caption title">
-								by one, get one <span class="primary">50% <strong>off</strong></span>
-							</h2>
-							<h4 class="caption subtitle">school supplies & backpacks.*</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-						</div>
-					</li>
-					<li><img src="/assets/site/img/h4-slide3.png" alt="Slide">
-						<div class="caption-group">
-							<h2 class="caption title">
-								Apple <span class="primary">Store <strong>Ipod</strong></span>
-							</h2>
-							<h4 class="caption subtitle">Select Item</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-						</div>
-					</li>
-					<li><img src="/assets/site/img/h4-slide4.png" alt="Slide">
-						<div class="caption-group">
-						  <h2 class="caption title">
-								Apple <span class="primary">Store <strong>Ipod</strong></span>
-							</h2>
-							<h4 class="caption subtitle">& Phone</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-						</div>
-					</li>
+                    <?php } ?>
 				</ul>
 			</div>
 			<!-- ./Slider -->
