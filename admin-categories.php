e6eb4c1817019f763a48fb96b850f8911c9660de
Carlos Rhedney <twisterpsa@hotmail.com>
2023-09-03 14:14:53 -0300

    Relation products for categories

--- admin-categories.php
+++ admin-categories.php
@@ -4,6 +4,7 @@ use Psr\Http\Message\ServerRequestInterface AS Request;
 use \Crhedstore\PageAdmin;
 use \Crhedstore\Model\User;
 use \Crhedstore\Model\Category;
+use \Crhedstore\Model\Product;
 
 $app->get('/admin/categories', function(Request $request, Response $response, array $args){
 
@@ -93,6 +94,64 @@ $app->post('/admin/categories/{idcategory}', function(Request $request, Response
 
 	exit;
 
+});
+
+$app->get('/admin/categories/{idcategory}/products', function(Request $request, Response $response, array $args){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$args["idcategory"]);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-products", [
+		"category"=>$category->getValues(),
+		"productsRelated"=>$category->getProducts(),
+		"productsNotRelated"=>$category->getProducts(false)
+	]);
+
+});
+
+$app->get('/admin/categories/{idcategory}/products/{idproduct}/add', function(Request $request, Response $response, array $args){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$args["idcategory"]);
+
+	$product = new Product();
+
+	$product->get((int)$args["idproduct"]);
+
+	$category->addProduct($product);
+
+	header("Location: /admin/categories/".$args["idcategory"]."/products");
+
+	exit;
+
+});
+
+$app->get('/admin/categories/{idcategory}/products/{idproduct}/remove', function(Request $request, Response $response, array $args){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$args["idcategory"]);
+
+	$product = new Product();
+
+	$product->get((int)$args["idproduct"]);
+
+	$category->removeProduct($product);
+
+	header("Location: /admin/categories/".$args["idcategory"]."/products");
+
+	exit;
+	
 });
 
 ?>
