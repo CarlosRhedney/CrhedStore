993ad6b321d6d0e7f988dca8ba836a6e066f165b
Carlos Rhedney <twisterpsa@hotmail.com>
2023-09-09 03:13:55 -0300

    Adding shopping cart part two

--- vendor/crhedstore/php-classes/src/Model/Cart.php
+++ vendor/crhedstore/php-classes/src/Model/Cart.php
@@ -105,5 +105,54 @@ class Cart extends Model
 		$this->setData($results[0]);
 
 	}
+
+	public function addProduct(Product $product)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("INSERT INTO tb_cartsproducts(idcart, idproduct) VALUES(:idcart, :idproduct)", [
+			":idcart"=>$this->getidcart(),
+			":idproduct"=>$product->getidproduct()
+		]);
+
+	}
+
+	public function removeProduct(Product $product, $all = false)
+	{
+
+		$sql = new Sql();
+
+		if($all){
+
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			]);
+		}else{
+
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			]);
+
+		}
+
+	}
+
+	public function getProducts()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT b.idproduct, b.product, b.price, b.width, b.height, b.length, b.weight, b.url, COUNT(*) AS nrqtd, SUM(b.price) AS vltotal FROM tb_cartsproducts a INNER JOIN tb_products b ON a.idproduct = b.idproduct WHERE a.idcart = :idcart AND a.dtremoved IS NULL GROUP BY b.idproduct, b.product, b.price, b.width, b.height, b.length, b.weight, b.url ORDER BY b.product", array(
+			":idcart"=>$this->getidcart()
+		));
+
+		return Product::checkList($results);
+		
+	}
+	
+
 }
 ?>
